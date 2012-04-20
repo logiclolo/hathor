5400c90f172780f70d9295a6b9470ba23bc9dd84
klaymen <klaymen@7a6c5977-b700-0410-9ebf-f0490b269a9c>
2012-04-20 05:50:36 +0000

    sync
    git-svn-id: http://rd1-1/subversion/SI/Tools/hathor/trunk@110509 7a6c5977-b700-0410-9ebf-f0490b269a9c

--- src/lib/actions.mk
+++ src/lib/actions.mk
@@ -13,7 +13,8 @@ _firmware_check:
 _firmware_release: 
 	@bin/firmware_prerelease
 
-_upload: product/history.txt product/firmware
+_upload:
 	@bin/rt_ticket_send
+	@bin/firmware_copy
 
 .PHONY: _tag _build_kernel _build_firmware _firmware_check _firmware_release _upload
